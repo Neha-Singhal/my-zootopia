2673b5562948428de3a51b8bfe1aa3ca10317a51
neha <nehasinghal@Nehas-MacBook-Air.local>
2025-01-28 23:26:15 +0100

    Bring animal data into HTML

--- animals_web_generators.py
+++ animals_web_generators.py
@@ -1,44 +1,48 @@
 import json
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
 
-def display_animal_info (animals):
+def display_animal_info(animal):
     output = []
-    for animal in animals:
-        if 'name' in animal:
-            output.append(f"Name: {animal ['name']}")
-        if 'characteristics' in animal:
-            characteristics = animal['characteristics']
-        if 'diet' in characteristics:
-            output.append(f"diet: {characteristics['diet']}")
-        if'locations' in animal and animal['locations']:
-            output.append(f"Location:{animal['locations'] [0]}")
-        if 'type' in characteristics:
-            output.append(f"type:{characteristics['type']}")
-        return "\n".join(output)
 
+    if 'name' in animal:
+        output.append(f"Name: {animal['name']}")
+    if 'characteristics' in animal:
+        characteristics = animal['characteristics']
+    if 'diet' in characteristics:
+        output.append(f"diet: {characteristics['diet']}")
+    if 'locations' in animal and animal['locations']:
+        output.append(f"Location:{animal['locations'][0]}")
+    if 'type' in characteristics:
+        output.append(f"type:{characteristics['type']}")
+    return "<br/>\n".join(output)
 
 def generate_animals_output(animals):
-    return "\n\n".join (display_animal_info(animals) for animal in animals)
-
+    items = []
+    for animal in animals:
+        animal_info = display_animal_info(animal)
+        item_html = f'<li class="cards__item">\n{animal_info}\n</li>'
+        items.append(item_html)
+    return "\n".join(items)
 
 def main():
     animals_data = load_data('animals_data.json')
     animals_output = generate_animals_output(animals_data)
 
-    with open('animals_template.html','r') as template_file:
+    with open('animals_template.html', 'r') as template_file:
         template_content = template_file.read()
 
     new_html_content = template_content.replace('__REPLACE_ANIMALS_INFO__', animals_output)
 
-    with open('animals.html','w')as output_file:
+    with open('animals.html', 'w') as output_file:
         output_file.write(new_html_content)
 
     print(animals_output)
 
-
 if __name__ == "__main__":
-    main()
+        main()
+
+
